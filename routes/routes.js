aed5cd2df29cce0f672e5db24270ea5b26c38c02
Bikin Maharjan <bikinmaharjan10@gmail.com>
2022-03-09 18:13:46 +1100

    GET Articles by tagName and date implemented. *Need to make changes on sorting and limit on articles *Need to remove queried tagName from related_tags

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const res = require('express/lib/response');
 const Article = require('../models/article');
 const router = express.Router();
+const moment = require('moment');
 
 //@desc     Create new Article
 //@route    POST  /articles
@@ -38,6 +40,58 @@ router.get('/articles/:id', async (req, res) => {
 
 //@desc     Articles by tagname and date
 //@route    GET /tags/:tagName/:date
-router.get('/tags/:tagName/:date', (req, res) => {});
+router.get('/tags/:tagName/:date', async (req, res) => {
+  try {
+    tagName = req.params.tagName;
+    reqDate = req.params.date;
+
+    dDate = moment(reqDate).format('YYYY-MM-DD');
+
+    start = moment(moment(dDate).startOf('day')).local();
+    end = moment(moment(dDate).endOf('day')).local();
+
+    const requiredCondition = {
+      $and: [
+        {
+          date: {
+            $gte: start,
+            $lte: end,
+          },
+          tags: tagName,
+        },
+      ],
+    };
+    //Getting Articles with the same date
+    articleByDateAndTag = await Article.find({
+      $and: [
+        {
+          date: {
+            $gte: start,
+            $lte: end,
+          },
+          tags: tagName,
+        },
+      ],
+    });
+
+    //Count of article for the date with the tag
+    const tagCount = await Article.count(requiredCondition);
+
+    const articleIdWithTag = await Article.distinct('_id', requiredCondition);
+
+    //Finding the related tags
+    const relatedTags = await Article.distinct('tags', requiredCondition);
+
+    res.status(200).json({
+      tag: tagName,
+      // articleByDateAndTag,
+      count: tagCount,
+      articles: articleIdWithTag,
+      related_tags: relatedTags,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
